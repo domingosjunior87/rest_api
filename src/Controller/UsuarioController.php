b1bf62804118c5a7e1fe74460138b9342bc02036
domingos.coelho <domingos@prodam.am.gov.br>
2021-08-24 17:29:59 -0400

    Implementado endpoint para listar usuários

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\UsuarioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +16,8 @@ class UsuarioController extends AbstractController
     /**
      * @Route("/", name="_get_all", methods={"GET"})
      */
-    public function index(): Response
+    public function index(UsuarioRepository $usuarioRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UsuarioController.php',
-        ]);
+        return $this->json($usuarioRepository->findAll());
     }
 }
